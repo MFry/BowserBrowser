a6ceb6baf0045578604eb19ba5530fd48c828e6a
Michal Frystacky <Michal.Frystacky@gmail.com>
2013-06-10 21:48:23 -0400

    Added the last three tokens to the javascript lexer

--- lexers/JavaScriptLexer.py
+++ lexers/JavaScriptLexer.py
@@ -40,7 +40,7 @@ tokens = (
         'FUNCTION',     # function
         'GE',           # >=
         'GT',           # >
-#       'IDENTIFIER',   #### Not used in this problem.
+       'IDENTIFIER',    # Defined below
         'IF',           # if
         'LBRACE',       # {
         'LE',           # <=
@@ -48,19 +48,21 @@ tokens = (
         'LT',           # <
         'MINUS',        # -
         'NOT',          # !
-#       'NUMBER',       #### Not used in this problem.
+       'NUMBER',        # Defined below
         'OROR',         # ||
         'PLUS',         # +
         'RBRACE',       # }
         'RETURN',       # return
         'RPAREN',       # )
         'SEMICOLON',    # ;
-#       'STRING',       #### Not used in this problem.
+       'STRING',        # Defined below
         'TIMES',        # *
         'TRUE',         # true
         'VAR',          # var
 )
 
+
+# The single comment state can be done by using the maximal munch rule and doesn't need a state of its own
 states = (
               ('commentSingle','exclusive'),
               ('commentMulti', 'exclusive'),
@@ -108,56 +110,88 @@ def t_commentMulti_error(t):
 #We employ writing these tokens in shorthand, thus:
 
 t_ANDAND = r'&&'
-
 t_COMMA = r','
-
 t_DIVIDE = r'/'
-
 t_ELSE = r'else'
-
 t_EQUAL = r'='
-
 t_EQUALEQUAL = r'=='
-
 t_FALSE = r'false'
-
 t_FUNCTION = r'function'
-
 t_GE = r'>='
-
 t_GT = r'>'
-
 t_IF = r'if'
-
 t_LBRACE = r'{'
-
 t_LE = r'<='
-
 t_LPAREN = r'\('
-
 t_LT = r'<'
-
 t_MINUS = r'-'
-
 t_NOT = r'!'
-
 t_OROR = r'\|\|'
-
 t_PLUS = r'\+'
-
 t_RBRACE = r'\}'
-
 t_RETURN = r'return'
-
 t_RPAREN = r'\)'
-
 t_SEMICOLON = r';'
-
 t_TIMES = r'\*'
-
 t_TRUE = r'true'
-
 t_VAR = r'var'
+
+# JavaScript: Numbers & Strings
+#
+# In this exercise you will finish out the token definitions for JavaScript
+# by handling Numbers, Identifiers and Strings.
+#
+# We have split the lexing of JavaScript into two exercises so that
+# you have a chance to demonstrate your mastery of the concepts
+# independently (i.e., so that you can get one of them right even if the
+# other proves difficult). We could easily make a full JavaScript lexer by
+# putting all of the rules together.
+#
+# For this assignment, a JavaScript IDENTIFIER must start with an upper- or
+# lower-case character. It can then contain any number of upper- or
+# lower-case characters or underscores. Its token.value is the textual
+# string of the identifier.
+#       Yes:    my_age
+#       Yes:    cRaZy
+#       No:     _starts_with_underscore
+#
+# For this assignment, a JavaScript NUMBER is one or more digits. A NUMBER
+# can start with an optional negative sign. A NUMBER can contain a decimal
+# point, which can then be followed by zero or more additional digits. Do
+# not worry about hexadecimal (only base 10 is allowed in this problem).
+# The token.value of a NUMBER is its floating point value (NOT a string).
+#       Yes:    123
+#       Yes:    -456
+#       Yes:    78.9
+#       Yes:    10.
+#       No:     +5
+#       No:     1.2.3
+#
+# For this assignment, a JavaScript STRING is zero or more characters
+# contained in double quotes. A STRING may contain escaped characters.
+# Notably, \" does not end a string. The token.value of a STRING is
+# its contents (not including the outer double quotes).
+#       Yes:    "hello world"
+#       Yes:    "this has \"escaped quotes\""
+#       No:     "no"t one string"
+#
+# Hint: float("2.3") = 2.3
+
+def t_STRING(token):
+    r'"([^"\\]|(\\.))*"'# My original solution that wasn't as robust r'\".*[^\\]\"'
+    token.value = token.value[1:-1]
+    return token
+
+def t_IDENTIFIER(token):
+    r'[a-zA-Z](?: [a-zA-Z_]+)?'
+    return token
+
+def t_NUMBER(token):
+    r'[-]?[0-9]+(?:\.[0-9]+)?'
+    token.value = float(token.value)
+    return token
+
+
 
 t_ignore = ' \t\v\r' # whitespace
 
@@ -202,3 +236,13 @@ true /* false
 output2 = ['IF', 'EQUAL', 'EQUAL', 'TRUE', 'RETURN']
 
 print (test_lexer(input2) == output2)
+
+input3 = 'some_identifier -12.34 "a \\"escape\\" b"'
+output3 = ['IDENTIFIER', 'some_identifier', 'NUMBER', -12.34, 'STRING',
+'a \\"escape\\" b']
+print (test_lexer(input3) == output3)
+
+
+input4 = '-12x34'
+output4 = ['NUMBER', -12.0, 'IDENTIFIER', 'x', 'NUMBER', 34.0]
+print (test_lexer(input4) == output4)
